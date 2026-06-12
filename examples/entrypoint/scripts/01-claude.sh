e9e35835f4b057672dcd0e6231e6dd06fbe07e41
Cristian Armesto <cristianarmestotejedo@gmail.com>
2026-06-12 18:50:56 +0200

    Libraries moved from Dockerfile to custom script

--- examples/entrypoint/scripts/01-claude.sh
+++ examples/entrypoint/scripts/01-claude.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
+# Installing libraries
+pipx install --force notebooklm-mcp-cli
+npm install -g ctx7
+curl -fsSL https://raw.githubusercontent.com/rtk-ai/rtk/refs/heads/master/install.sh | sh
+
+# Skills
 wget https://github.com/lugasia/3gpp-skill/releases/download/v1.1.0/3gpp-expert.skill
 mkdir 3gpp
 mv 3gpp-expert.skill 3gpp/
